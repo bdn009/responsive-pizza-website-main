543a1c508aaf12f02f9414bc0bdf9c0129d91088
Ashikur Rahman <ashikurrahaman236@gmail.com>
2025-03-02 06:23:01 +0600

    nav done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,20 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('navMenu'),
+      navToggle = document.getElementById('navToggle'),
+      navClose = document.getElementById('navClose')
 
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
