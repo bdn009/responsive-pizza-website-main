255be0e7c983392653b47e79dcef7000ac150e27
Ashikur Rahman <ashikurrahaman236@gmail.com>
2025-03-05 17:25:47 +0600

    recipe section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,13 +17,28 @@ if(navClose){
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.navLink')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('navMenu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD SHADOW HEADER ===============*/
-
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('headerShadow') 
+                       : header.classList.remove('headerShadow')
+}
+window.addEventListener('scroll', scrollHeader)
 /*=============== SWIPER POPULAR ===============*/
-
+const popularSwiper = new Swiper('.popularSwiper', {
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+  })
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
